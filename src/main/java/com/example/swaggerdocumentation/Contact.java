16a190cbaedc492d7ffc008ece522c6c6f747fa4
Taiwo Abdulahi <ataiwo@verifynigeria.com>
2024-01-10 11:56:42 +0100

    first commit

---
+++ src/main/java/com/example/swaggerdocumentation/Contact.java
@@ -0,0 +1,32 @@
+package com.example.swaggerdocumentation;
+
+public class Contact {
+    public String Id;
+    public String name;
+    public String phone;
+
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
